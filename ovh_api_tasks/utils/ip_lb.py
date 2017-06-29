9943b7854f9c3b451c45801f24dc2b27890a0297
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-06-29 16:54:54 +0200

    Remove code about next gen module

--- ovh_api_tasks/utils/ip_lb.py
+++ ovh_api_tasks/utils/ip_lb.py
@@ -2,7 +2,6 @@
 Utils function for IP LBs
 """
 
-from ovh_api_tasks.api_wrappers import ip_loadbalancing as ip_lb_next_gen
 from ovh_api_tasks.api_wrappers import ip as ip_lb_legacy
 from tenacity import retry, TryAgain
 from tenacity.stop import stop_after_delay
@@ -17,8 +16,8 @@ def waiting_ip_lb_service_tasks_done(ovh_client, service, lb_type):  # pylint: d
 
     if lb_type == 'legacy':
         tasks = ip_lb_legacy.get_ip_lb_service_tasks(ovh_client, service)
-    elif lb_type == 'next_gen':
-        tasks = ip_lb_next_gen.get_ip_lb_service_tasks(ovh_client, service)
+    else:
+        raise Exception('Bad value for Load Balancing type')
 
     tasks_count = len(tasks)
 
