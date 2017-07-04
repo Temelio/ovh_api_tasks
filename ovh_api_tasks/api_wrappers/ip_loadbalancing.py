ee77809085fb33466d0f469ca3a454900c879219
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-07-04 14:50:00 +0200

    Add some /ipLoadbalancing API wrappers

--- ovh_api_tasks/api_wrappers/ip_loadbalancing.py
+++ ovh_api_tasks/api_wrappers/ip_loadbalancing.py
@@ -143,3 +143,386 @@ def get_ip_lb_service_task(ovh_client, service, task_id):
     url = '/ipLoadbalancing/{service}/task/{id}'
 
     return ovh_client.get(url.format(service=quote_plus(service), id=task_id))
+
+
+def get_ip_lb_service_http_farms(ovh_client, service, zone=None):
+    """
+    Get next gen load balancing service HTTP farms
+
+    GET /ip/LoadBalancing/{serviceName}/http/farm
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm'
+
+    if zone:
+        return ovh_client.get(url.format(service=quote_plus(service)), zone=zone)
+
+    return ovh_client.get(url.format(service=quote_plus(service)))
+
+
+def get_ip_lb_service_http_farm(ovh_client, service, farm):
+    """
+    Get next gen load balancing service HTTP farm
+
+    GET /ip/LoadBalancing/{serviceName}/http/farm/{farmId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm))
+
+
+def add_ip_lb_service_http_farm(ovh_client, service, farm_data):
+    """
+    Add next gen load balancing service HTTP farm
+
+    POST /ip/LoadBalancing/{serviceName}/http/farm
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), **farm_data)
+
+
+def del_ip_lb_service_http_farm(ovh_client, service, farm):
+    """
+    Remove next gen load balancing service HTTP farm
+
+    DELETE /ip/LoadBalancing/{serviceName}/http/farm/{farmId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), farm=farm))
+
+
+def get_ip_lb_service_http_farm_servers(ovh_client, service, farm):
+    """
+    Get next gen load balancing service HTTP farm servers
+
+    GET /ip/LoadBalancing/{serviceName}/http/farm/{farmId}/server
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}/server'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm))
+
+
+def get_ip_lb_service_http_farm_server(ovh_client, service, farm, server):
+    """
+    Get next gen load balancing service HTTP farm server
+
+    GET /ip/LoadBalancing/{serviceName}/http/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}/server/{server}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm, server=server))
+
+
+def add_ip_lb_service_http_farm_server(ovh_client, service, farm, server_data):
+    """
+    Add next gen load balancing service HTTP farm server
+
+    POST /ip/LoadBalancing/{serviceName}/http/farm/{farmId}/server
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}/server'
+
+    return ovh_client.post(url.format(service=quote_plus(service), farm=farm), **server_data)
+
+
+def update_ip_lb_service_http_farm_server(ovh_client, service, farm, server, server_data):
+    """
+    Update next gen load balancing service HTTP farm server
+
+    PUT /ip/LoadBalancing/{serviceName}/http/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}/server/{server}'
+
+    return ovh_client.put(
+        url.format(service=quote_plus(service), farm=farm, server=server),
+        **server_data)
+
+
+def del_ip_lb_service_http_farm_server(ovh_client, service, farm, server):
+    """
+    Remove next gen load balancing service HTTP farm server
+
+    DELETE /ip/LoadBalancing/{serviceName}/http/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/farm/{farm}/server/{server}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), farm=farm, server=server))
+
+
+def get_ip_lb_service_http_frontends(ovh_client, service):
+    """
+    Get next gen load balancing service HTTP frontends
+
+    GET /ip/LoadBalancing/{serviceName}/http/frontend
+    """
+
+    url = '/ipLoadbalancing/{service}/http/frontend'
+
+    return ovh_client.get(url.format(service=quote_plus(service)))
+
+
+def get_ip_lb_service_http_frontend(ovh_client, service, frontend):
+    """
+    Get next gen load balancing service HTTP frontend
+
+    GET /ip/LoadBalancing/{serviceName}/http/frontend/{frontendId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/frontend/{frontend}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), frontend=frontend))
+
+
+def add_ip_lb_service_http_frontend(ovh_client, service, frontend_data):
+    """
+    Add next gen load balancing service HTTP frontend
+
+    POST /ip/LoadBalancing/{serviceName}/http/frontend
+    """
+
+    url = '/ipLoadbalancing/{service}/http/frontend'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), **frontend_data)
+
+
+def del_ip_lb_service_http_frontend(ovh_client, service, frontend):
+    """
+    Remove next gen load balancing service HTTP frontend
+
+    DELETE /ip/LoadBalancing/{serviceName}/http/frontend/{frontendId}
+    """
+
+    url = '/ipLoadbalancing/{service}/http/frontend/{frontend}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), frontend=frontend))
+
+
+def get_ip_lb_service_tcp_farms(ovh_client, service, zone=None):
+    """
+    Get next gen load balancing service HTTP farms
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/farm
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm'
+
+    if zone:
+        return ovh_client.get(url.format(service=quote_plus(service)), zone=zone)
+
+    return ovh_client.get(url.format(service=quote_plus(service)))
+
+
+def get_ip_lb_service_tcp_farm(ovh_client, service, farm):
+    """
+    Get next gen load balancing service HTTP farm
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm))
+
+
+def add_ip_lb_service_tcp_farm(ovh_client, service, farm_data):
+    """
+    Add next gen load balancing service HTTP farm
+
+    POST /ip/LoadBalancing/{serviceName}/tcp/farm
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), **farm_data)
+
+
+def del_ip_lb_service_tcp_farm(ovh_client, service, farm):
+    """
+    Remove next gen load balancing service HTTP farm
+
+    DELETE /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), farm=farm))
+
+
+def get_ip_lb_service_tcp_farm_servers(ovh_client, service, farm):
+    """
+    Get next gen load balancing service HTTP farm servers
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}/server
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}/server'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm))
+
+
+def get_ip_lb_service_tcp_farm_server(ovh_client, service, farm, server):
+    """
+    Get next gen load balancing service HTTP farm server
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}/server/{server}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), farm=farm, server=server))
+
+
+def add_ip_lb_service_tcp_farm_server(ovh_client, service, farm, server_data):
+    """
+    Add next gen load balancing service HTTP farm server
+
+    POST /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}/server
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}/server'
+
+    return ovh_client.post(url.format(service=quote_plus(service), farm=farm), **server_data)
+
+
+def update_ip_lb_service_tcp_farm_server(ovh_client, service, farm, server, server_data):
+    """
+    Update next gen load balancing service HTTP farm server
+
+    PUT /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}/server/{server}'
+
+    return ovh_client.put(
+        url.format(service=quote_plus(service), farm=farm, server=server),
+        **server_data)
+
+
+def del_ip_lb_service_tcp_farm_server(ovh_client, service, farm, server):
+    """
+    Remove next gen load balancing service HTTP farm server
+
+    DELETE /ip/LoadBalancing/{serviceName}/tcp/farm/{farmId}/server/{serverId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/farm/{farm}/server/{server}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), farm=farm, server=server))
+
+
+def get_ip_lb_service_tcp_frontends(ovh_client, service):
+    """
+    Get next gen load balancing service HTTP frontends
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/frontend
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/frontend'
+
+    return ovh_client.get(url.format(service=quote_plus(service)))
+
+
+def get_ip_lb_service_tcp_frontend(ovh_client, service, frontend):
+    """
+    Get next gen load balancing service HTTP frontend
+
+    GET /ip/LoadBalancing/{serviceName}/tcp/frontend/{frontendId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/frontend/{frontend}'
+
+    return ovh_client.get(url.format(service=quote_plus(service), frontend=frontend))
+
+
+def add_ip_lb_service_tcp_frontend(ovh_client, service, frontend_data):
+    """
+    Add next gen load balancing service HTTP frontend
+
+    POST /ip/LoadBalancing/{serviceName}/tcp/frontend
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/frontend'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), **frontend_data)
+
+
+def del_ip_lb_service_tcp_frontend(ovh_client, service, frontend):
+    """
+    Remove next gen load balancing service HTTP frontend
+
+    DELETE /ip/LoadBalancing/{serviceName}/tcp/frontend/{frontendId}
+    """
+
+    url = '/ipLoadbalancing/{service}/tcp/frontend/{frontend}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), frontend=frontend))
+
+
+def refresh_ip_lb_service(ovh_client, service, zone=None):
+    """
+    Apply next gen load balancing service configuration
+
+    POST /ip/LoadBalancing/{serviceName}/refresh
+    """
+
+    url = '/ipLoadbalancing/{service}/refresh'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), zone=zone)
+
+
+def get_ip_lb_service_ssl_items(ovh_client, service):
+    """
+    Get next gen load balancing service SSL items
+
+    GET /ip/LoadBalancing/{serviceName}/ssl
+    """
+
+    url = '/ipLoadbalancing/{service}/ssl'
+
+    return ovh_client.get(url.format(service=quote_plus(service)))
+
+
+def get_ip_lb_service_ssl_item(ovh_client, service, ssl_item_id):
+    """
+    Get next gen load balancing service SSL item
+
+    GET /ip/LoadBalancing/{serviceName}/ssl/{id}
+    """
+
+    url = '/ipLoadbalancing/{service}/ssl/{ssl_item_id}'
+
+    return ovh_client.get(
+        url.format(service=quote_plus(service), ssl_item_id=ssl_item_id))
+
+
+def add_ip_lb_service_ssl_item(ovh_client, service, ssl_item_data):
+    """
+    Add next gen load balancing service SSL item
+
+    POST /ip/LoadBalancing/{serviceName}/ssl
+    """
+
+    url = '/ipLoadbalancing/{service}/ssl'
+
+    return ovh_client.post(url.format(service=quote_plus(service)), **ssl_item_data)
+
+
+def del_ip_lb_service_ssl_item(ovh_client, service, ssl_item_id):
+    """
+    Remove next gen load balancing service SSL item
+
+    DELETE /ip/LoadBalancing/{serviceName}/ssl/{id}
+    """
+
+    url = '/ipLoadbalancing/{service}/ssl/{ssl_item_id}'
+
+    return ovh_client.delete(url.format(service=quote_plus(service), ssl_item_id=ssl_item_id))
--- tests/api_wrappers/test_ip_loadbalancing_wrapper.py
+++ tests/api_wrappers/test_ip_loadbalancing_wrapper.py
@@ -164,3 +164,434 @@ def test_get_ip_lb_service_task(mocker):
     ip_lb.get_ip_lb_service_task(ovh_client, 'ip-10.0.0.1', 1234)
     ovh_client.call.assert_called_with(
         'GET', '/ipLoadbalancing/ip-10.0.0.1/task/1234', None, True)
+
+
+def test_get_ip_lb_service_http_farms(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_farms function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_farms(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/farm', None, True)
+
+
+def test_get_ip_lb_service_http_farms_with_params(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_farms function with zone param
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_farms(ovh_client, 'ip-10.0.0.1', zone='foo')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/farm?zone=foo', None, True)
+
+
+def test_get_ip_lb_service_http_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_farm(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234', None, True)
+
+
+def test_add_ip_lb_service_http_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_http_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_http_farm(ovh_client, 'ip-10.0.0.1', {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/http/farm', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_http_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_http_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_http_farm(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234', None, True)
+
+
+def test_get_ip_lb_service_http_farm_servers(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_farm_servers function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_farm_servers(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234/server', None, True)
+
+
+def test_get_ip_lb_service_http_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_farm_server(ovh_client, 'ip-10.0.0.1', 1234, 5678)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234/server/5678', None, True)
+
+
+def test_add_ip_lb_service_http_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_http_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_http_farm_server(ovh_client, 'ip-10.0.0.1', 1234, {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234/server', {'foo': 'bar'}, True)
+
+
+def test_update_ip_lb_service_http_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test update_ip_lb_service_http_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.update_ip_lb_service_http_farm_server(
+        ovh_client, 'ip-10.0.0.1', 1234, 5678, {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'PUT', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234/server/5678', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_http_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_http_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_http_farm_server(ovh_client, 'ip-10.0.0.1', 1234, 5678)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/http/farm/1234/server/5678', None, True)
+
+
+def test_get_ip_lb_service_http_frontends(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_frontends function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_frontends(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/frontend', None, True)
+
+
+def test_get_ip_lb_service_http_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_http_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_http_frontend(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/http/frontend/1234', None, True)
+
+
+def test_add_ip_lb_service_http_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_http_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_http_frontend(ovh_client, 'ip-10.0.0.1', {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/http/frontend', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_http_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_http_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_http_frontend(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/http/frontend/1234', None, True)
+
+
+def test_get_ip_lb_service_tcp_farms(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_farms function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_farms(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm', None, True)
+
+
+def test_get_ip_lb_service_tcp_farms_with_params(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_farms function with zone param
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_farms(ovh_client, 'ip-10.0.0.1', zone='foo')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm?zone=foo', None, True)
+
+
+def test_get_ip_lb_service_tcp_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_farm(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234', None, True)
+
+
+def test_add_ip_lb_service_tcp_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_tcp_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_tcp_farm(ovh_client, 'ip-10.0.0.1', {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_tcp_farm(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_tcp_farm function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_tcp_farm(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234', None, True)
+
+
+def test_get_ip_lb_service_tcp_farm_servers(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_farm_servers function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_farm_servers(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234/server', None, True)
+
+
+def test_get_ip_lb_service_tcp_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_farm_server(ovh_client, 'ip-10.0.0.1', 1234, 5678)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234/server/5678', None, True)
+
+
+def test_add_ip_lb_service_tcp_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_tcp_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_tcp_farm_server(ovh_client, 'ip-10.0.0.1', 1234, {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234/server', {'foo': 'bar'}, True)
+
+
+def test_update_ip_lb_service_tcp_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test update_ip_lb_service_tcp_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.update_ip_lb_service_tcp_farm_server(
+        ovh_client, 'ip-10.0.0.1', 1234, 5678, {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'PUT', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234/server/5678', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_tcp_farm_server(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_tcp_farm_server function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_tcp_farm_server(ovh_client, 'ip-10.0.0.1', 1234, 5678)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/tcp/farm/1234/server/5678', None, True)
+
+
+def test_get_ip_lb_service_tcp_frontends(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_frontends function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_frontends(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/frontend', None, True)
+
+
+def test_get_ip_lb_service_tcp_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_tcp_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_tcp_frontend(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/tcp/frontend/1234', None, True)
+
+
+def test_add_ip_lb_service_tcp_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_tcp_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_tcp_frontend(ovh_client, 'ip-10.0.0.1', {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/tcp/frontend', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_tcp_frontend(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_tcp_frontend function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_tcp_frontend(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/tcp/frontend/1234', None, True)
+
+
+def test_refresh_ip_lb_service(mocker):  # pylint: disable=invalid-name
+    """
+    Test refresh_ip_lb_service function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.refresh_ip_lb_service(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/refresh', {'zone': None}, True)
+
+
+def test_get_ip_lb_service_ssl_items(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_ssl_items function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_ssl_items(ovh_client, 'ip-10.0.0.1')
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/ssl', None, True)
+
+
+def test_get_ip_lb_service_ssl_item(mocker):  # pylint: disable=invalid-name
+    """
+    Test get_ip_lb_service_ssl_item function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.get_ip_lb_service_ssl_item(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'GET', '/ipLoadbalancing/ip-10.0.0.1/ssl/1234', None, True)
+
+
+def test_add_ip_lb_service_ssl_item(mocker):  # pylint: disable=invalid-name
+    """
+    Test add_ip_lb_service_ssl_item function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.add_ip_lb_service_ssl_item(ovh_client, 'ip-10.0.0.1', {'foo':'bar'})
+    ovh_client.call.assert_called_with(
+        'POST', '/ipLoadbalancing/ip-10.0.0.1/ssl', {'foo': 'bar'}, True)
+
+
+def test_del_ip_lb_service_ssl_item(mocker):  # pylint: disable=invalid-name
+    """
+    Test del_ip_lb_service_ssl_item function
+    """
+
+    mocker.patch('ovh.client.Client.call')
+    ovh_client = ovh.Client()
+
+    ip_lb.del_ip_lb_service_ssl_item(ovh_client, 'ip-10.0.0.1', 1234)
+    ovh_client.call.assert_called_with(
+        'DELETE', '/ipLoadbalancing/ip-10.0.0.1/ssl/1234', None, True)
